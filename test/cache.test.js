487ddd3962ca27230b978cc15078f028bf4f8a54
Dreamyplayer <saikumarchinna123@gmail.com>
2021-06-15 16:39:03 +0530

    revert back to old version

--- test/cache.test.js
+++ test/cache.test.js
@@ -10,7 +10,8 @@ test.serial('Adapters (Cache)', async t => {
   t.is(await dreamy.set('foo', 'bar'), true);
   t.is(await dreamy.get('foo'), 'bar');
   t.is(await dreamy.set('profile', { dev: true, user: 'dreamy' }), true);
-  t.is(await dreamy.set('profile', false, 'dev'), true);
+  t.is(await dreamy.set('profile', 'dev', false), true);
+  t.is(await dreamy.get('profile', 'dev'), true);
   t.is(await dreamy.find(f => f === 'oof'), undefined);
   t.is(await dreamy.get('profile', 'verified'), undefined);
   t.is(store.size, 2);
